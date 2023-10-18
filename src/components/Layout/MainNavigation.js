96ce70829d22901c26aa64a42ac803f13ab54114
humblesmarts <humblesmarts11@gmail.com>
2023-10-18 22:12:12 +0100

    fixed some bug

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -15,7 +15,7 @@ const MainNavigation = () => {
   return (
     <header className={classes.header}>
       <Link to="/">
-        <div className={classes.logo}>React Auth</div>
+        <div className={classes.logo}>Login Auth</div>
       </Link>
       <nav>
         <ul>
